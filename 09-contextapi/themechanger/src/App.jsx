a6ffb29e15f932630b21d1b2bc24e22a64118df8
Anirban Sarkar <anirbansarkar.slg18@gmail.com>
2025-03-22 03:51:15 +0530

    fixed th e dark mode in new tailwind version

--- 09-contextapi/themechanger/src/App.jsx
+++ 09-contextapi/themechanger/src/App.jsx
@@ -1,40 +1,46 @@
-import { useEffect, useState } from "react";
 
-import "./App.css";
-import { ThemeProvider } from "./context/Theme";
-import ThemeBtn from "./components/Theme";
-import Card from "./components/Card";
+import { useEffect, useState } from 'react'
+import './App.css'
+import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
-  const [themeMode , setThemeMode] = useState("dark")
+  const [themeMode, setThemeMode] = useState("dark")
 
-  const lightTheme = ()=>{
+  const lightTheme = () => {
     setThemeMode("light")
   }
-  const darkTheme = ()=>{
+
+  const darkTheme = () => {
     setThemeMode("dark")
   }
-  // actual theme change in thee 
-  useEffect(()=>{
 
-    document.querySelector("html").classList.remove("light","dark")
-    document.querySelector("html").classList.add(themeMode)
-  },[themeMode])
+  // actual change in theme
+
+  useEffect(() => {
+    localStorage.setItem("theme", themeMode);
+    const html = document.documentElement;
+    html.classList.remove("light", "dark");
+    html.classList.add(themeMode);
+  }, [themeMode]);
+  
 
   return (
-    <ThemeProvider value={{themeMode,darkTheme , lightTheme}}>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
       <div className="flex flex-wrap min-h-screen items-center">
-        <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/*  theme btn*/}
-            <ThemeBtn/>
-          </div>
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+              </div>
 
-          <div className="w-full max-w-sm mx-auto">{/* cards  */}<Card/></div>
-        </div>
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
       </div>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
